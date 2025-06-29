7be32f42c0070dd86bdc32fd8b1fcb8080d639bc
Simen <97337442+simen64@users.noreply.github.com>
2025-06-29 23:05:00 +0200

    fix: downlaod spec file and some minor changes

--- copr_script.sh
+++ copr_script.sh
@@ -9,16 +9,25 @@ set -euo pipefail
 PKGNAME="netfoil"
 VERSION="1.0"
 SRC_REPO="https://github.com/tinfoil-factory/$PKGNAME.git"
+SPEC_REPO="https://github.com/simen64/$PKGNAME-rpm"
 SPEC_FILE="$PKGNAME.spec"
 
 # Get source
 git clone "$SRC_REPO" "$PKGNAME-$VERSION"
 rm -rf "$PKGNAME-$VERSION/.git"
 
+# Get go dependencies
+cd "$PKGNAME-$VERSION"/ 
+go mod vendor
+cd ..
+
 # Create tarball
 tar -czf "$PKGNAME-$VERSION.tar.gz" "$PKGNAME-$VERSION"
 rm -rf "$PKGNAME-$VERSION"
 
-# Get RPM
-mv netfoil-rpm/$PKGNAME.spec ./
+# Get spec
+git clone "$SPEC_REPO"
+
+# Cleanup
+mv $PKGNAME-rpm/$PKGNAME.spec ./
 rm -rf "$PKGNAME-rpm"
